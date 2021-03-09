1502a138dd95854b8a3887bd50e9a29832714528
Maria Afanaseva <mashenkachukina@gmail.com>
2021-03-09 19:32:52 +0100

    Add pagination on frontend and not found jobs message.

--- static/vue_js/index.js
+++ static/vue_js/index.js
@@ -1,14 +1,51 @@
+Vue.component('pages', {
+    props: {
+        totalPages: Array,
+    },
+    emits: ['update'],
+
+    template:`
+        <div class="pages">
+            <div class="page-number" v-for="page in totalPages" :key="page" >
+                <div v-if="page" v-on:click="$emit('update', page)">
+                    {{ page }}
+                </div>
+                <div v-else>
+                    ...
+                </div>
+            </div>
+        </div>
+    `
+});
+
 Vue.component('vacancies', {
     props: {
-        jobs: Array
+        jobs: Array,
     },
 
-    template:
-        '<div>' +
-            '<a v-for="job in jobs" :key="job.id" :href="`/jobs/${job.id}`">' +
-                '<div v-html="job.title"></div>' +
-            '</a>' +
-        '</div>'
+    template:`
+        <div>
+            <div>
+                <a v-for="job in jobs" :key="job.id" :href="'/jobs/' + job.id" target="_blank">
+                    <div style="padding: 20px;" v-html="job.title"></div>
+                </a>
+            </div>
+        </div>    
+    `
+});
+
+Vue.component('message', {
+    props: {
+        isVisible: Boolean,
+    },
+
+    template:`
+        <div>
+            <div v-show="isVisible">
+                Für Deine Suche haben wir keine aktuelle Stellenangebote gefunden.
+            </div>
+        </div>    
+    `
 });
 
 new Vue({
@@ -17,7 +54,9 @@ new Vue({
     data() {
         return{
             jobs: [],
-            searchLine: ''
+            searchLine: '',
+            totalPages: [],
+            isVisibleMessage: false,
         }
     },
 
@@ -26,15 +65,30 @@ new Vue({
     },
 
     methods: {
-        getData() {
-            let url = 'http://127.0.0.1:5000/search?';
+        checkData(data) {
+            this.isVisibleMessage = (data.length === 0);
+        },
+
+        scrollToTop() {
+            window.scrollTo(0,0);
+        },
+
+        getData(page=1) {
+            let url = 'http://127.0.0.1:5000/search?page=' + page;
+            console.log(url);
             let keywords = this.searchLine.match(/[^ ]+/g);
             if (keywords) {
                 url += '&keyword=' + keywords.join('&keyword=');
             }
             fetch(url)
                 .then(res => res.json())
-                .then(result => this.jobs = result.jobs)
+                .then(result => {
+                    this.checkData(result.jobs);
+                    this.jobs = result.jobs;
+                    this.totalPages = result.totalPages
+                });
+            this.scrollToTop();
+
         }
     }
 });
